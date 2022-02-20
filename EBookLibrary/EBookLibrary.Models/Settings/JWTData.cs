d68fe5f864b04bcb5ee3356f5021d3ed953a17ae
Israel Oluwafemi <israeloluwafemi44@gmail.com>
2022-02-20 18:33:20 +0100

    Ready to push

---
+++ EBookLibrary/EBookLibrary.Models/Settings/JWTData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EBookLibrary.Models.Settings
+{
+    public class JWTData
+    {
+        public const string Data = "JWTData";
+        public TimeSpan TokenLifeTime { get; set; }
+
+        public string SecretKey { get; set; }
+
+        public string Issuer { get; set; }
+
+        public string Audience { get; set; }
+    }
+}
